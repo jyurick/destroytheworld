ecc6024477bb57a751d71ccf9da9c10a0e040d45
Anna Holowaychuk <aholoway@ualberta.ca>
2016-10-15 23:41:56 -0600

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Anna Holowaychuk on 2016-10-15.
+//  Copyright © 2016 Jamie Yurick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
